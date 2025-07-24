17113f00f9f7483ed311e8dda7c5c7f695758601
yasuonagaro <yasuo.nagaro@gmail.com>
2025-07-24 20:17:31 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by ynagaro on 2025/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecondKadaiAppTests/SecondKadaiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondKadaiAppTests.swift
+//  SecondKadaiAppTests
+//
+//  Created by ynagaro on 2025/07/24.
+//
+
+import Testing
+@testable import SecondKadaiApp
+
+struct SecondKadaiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
